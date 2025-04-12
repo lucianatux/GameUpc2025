4b0278fab75c937eb8a802080e72ae9dad0198a9
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-04-12 16:24:49 -0300

    Room Manager
    Room manager y rooms scripts creados, falta el de cada room

---
+++ Assets/Scripts/Utils/RoomManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class RoomManager : MonoBehaviour
+{
+    public static RoomManager Instance => _instance;
+    private static RoomManager _instance;
+
+    private int _currentRoomID;
+    public int CurrentRoomID => _currentRoomID;
+
+    public event Action<int> OnRoomEntered;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            _instance = this; // si no hay una instancia, que la cree
+            return;
+        }
+
+        Destroy(gameObject); // si hay una instancia, que la destruya
+    }
+
+    public void SetCurrentRoom(int roomID)
+    {
+        _currentRoomID = roomID; // dato a enviar 
+        Debug.Log("Jugador entró a la room " + roomID);
+        OnRoomEntered?.Invoke(roomID); // Evento para que lo escuchen, envia room ID
+    }
+}
