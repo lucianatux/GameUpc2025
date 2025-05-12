dd5378f76f4ff296e6d51240d9f49cd7f4744f14
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-05-12 15:27:10 -0300

    Revert "Merge branch 'feature/enemyAi' into feature/AnimationsManager"
    This reverts commit f3a99176d780783ddbac178a462c20499e70b5b0, reversing
    changes made to 481a81a11c7024e083cd79eb12052f8c1d3c7c64.

--- Assets/Scripts/EnemiesScripts/IEnemyState.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace StatePattern
-{
-public interface IEnemyState
-{
-    void EnterState (EnemyAI _enemyAI);
-    void UpdateState();
-}
-}
--- Assets/Scripts/Key.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Key", menuName = "Items/Key")]
-public class Key : ScriptableObject
-{
-    public string id;
-    public string displayName;
-    public Sprite icon;
-}
