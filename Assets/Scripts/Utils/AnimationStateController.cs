806be791994a39ab20f1d4fe31f5747748b3d339
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-05-12 15:26:52 -0300

    Revert "add animationcontroller to Player Movement"
    This reverts commit 4b24a0802a7c2477f69a8ea33371c0fd0aa7eb25.

--- Assets/Scripts/Utils/AnimationStateController.cs
+++ Assets/Scripts/Utils/AnimationStateController.cs
@@ -1,37 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class AnimationStateController : MonoBehaviour
 {
-    private Animator animator;
-    private AnimName currentState;
-    private int currentPriority;
-    private bool isLocked;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        animator = GetComponent<Animator>();
+        
     }
 
-    public void Play(AnimName newState, int priority = 0, bool lockState = false)
+    // Update is called once per frame
+    void Update()
     {
-        if (isLocked && priority < currentPriority) return;
-        if (newState == currentState) return;
-        if (priority < currentPriority) return;
-        animator.Play(newState.ToAnimString());
-        currentState = newState;
-        currentPriority = priority;
-        isLocked = lockState;
-    }
-
-    public void Unlock()
-    {
-        isLocked = false;
-        currentPriority = 0;
-    }
-
-    public bool IsPlaying(AnimName state)
-    {
-        return currentState == state;
+        
     }
 }
