cb90e54207250e5d9a02ddc1c4af99355da4d290
Lucianatux <lucianatux@yahoo.com.ar>
2025-06-10 17:12:06 -0300

    add thought system with ScriptableObjects and world-space UI

---
+++ Assets/Scripts/Player/ThoughtSystem/ThoughtManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThoughtManager : MonoBehaviour
+{
+    public static ThoughtManager Instance;
+
+    public GameObject thoughtBubbleUI;
+    //public TextMeshProUGUI thoughtText;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        thoughtBubbleUI.SetActive(false);
+    }
+
+    public void ShowThought(ThoughtSO thought)
+    {
+        StopAllCoroutines(); // Por si ya se estaba mostrando uno
+        StartCoroutine(DisplayThought(thought));
+    }
+
+    private IEnumerator DisplayThought(ThoughtSO thought)
+    {
+        //thoughtText.text = thought.text;
+        thoughtBubbleUI.SetActive(true);
+
+        yield return new WaitForSeconds(thought.duration);
+
+        thoughtBubbleUI.SetActive(false);
+    }
+}
