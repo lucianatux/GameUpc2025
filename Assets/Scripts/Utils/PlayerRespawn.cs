7d0e3f92397bfdf91de6b36b86833f14ab2c28d8
Lucianatux <lucianatux@yahoo.com.ar>
2025-06-15 13:11:17 -0300

    Fix: Player movement and physics now reactivate on respawn.

--- Assets/Scripts/Utils/PlayerRespawn.cs
+++ Assets/Scripts/Utils/PlayerRespawn.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRespawn : MonoBehaviour
@@ -32,5 +34,22 @@ public class PlayerRespawn : MonoBehaviour
         {
             life.ResetHealth();
         }
-    } 
+        // Reactivar movimiento y físicas
+        PlayerMovement move = GetComponent<PlayerMovement>();
+        if (move != null)
+            move.canMove = true;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+            rb.bodyType = RigidbodyType2D.Dynamic;
+
+        Collider2D col = GetComponent<Collider2D>();
+        if (col != null)
+            col.enabled = true;
+
+        Debug.Log("Jugador respawneado.");
+
+        PlayerEventsManager.Instance?.PlayerHeal(); // ← Esto fuerza el update de la barra de vida
+        } 
+        
 }
