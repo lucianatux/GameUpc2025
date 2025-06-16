7ac7887b4ec4f9ab0b6d8335bdf53bcd64a0a9f7
Lucianatux <lucianatux@yahoo.com.ar>
2025-06-16 16:41:00 -0300

    fix kick chizito

---
+++ Assets/Scripts/EnvironmentScripts/AutoCloseDoor.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCloseDoor : MonoBehaviour
+{
+    [SerializeField] private string playerTag = "Player";
+    [SerializeField] private Animator doorAnimator;
+
+    private BoxCollider2D solidCollider;
+    private bool alreadyClosed = false;
+
+    private void Awake()
+    {
+        // Busca el BoxCollider2D no trigger en este GameObject
+        BoxCollider2D[] colliders = GetComponents<BoxCollider2D>();
+        foreach (var col in colliders)
+        {
+            if (!col.isTrigger)
+            {
+                solidCollider = col;
+                solidCollider.enabled = false; // Asegura que esté desactivado al inicio
+                Debug.Log("[Puerta] Collider sólido encontrado y desactivado.");
+                break;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (alreadyClosed) return;
+        Debug.Log("[Puerta] Algo entró al trigger: " + other.name);
+
+        if (other.CompareTag(playerTag))
+        {
+            Debug.Log("[Puerta] Jugador detectado. Cerrando puerta...");
+            CloseDoor();
+        }
+    }
+
+    private void CloseDoor()
+    {
+        if (doorAnimator != null)
+        {
+            Debug.Log("[Puerta] Trigger de animación enviado.");
+            doorAnimator.SetTrigger("Close");
+        }
+        else
+        {
+            Debug.LogWarning("[Puerta] Animator no asignado.");
+        }
+
+        if (solidCollider != null)
+        {
+            solidCollider.enabled = true;
+            Debug.Log("[Puerta] Collider sólido activado.");
+        }
+
+        alreadyClosed = true;
+    }
+}
