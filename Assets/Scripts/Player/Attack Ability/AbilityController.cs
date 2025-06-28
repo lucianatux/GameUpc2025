01ae8f7499f83450fda7a3cb602796f43a4aa977
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-06-28 17:01:31 -0300

    add aji vfx

--- Assets/Scripts/Player/Attack Ability/AbilityController.cs
+++ Assets/Scripts/Player/Attack Ability/AbilityController.cs
@@ -18,12 +18,14 @@ public class AbilityController : MonoBehaviour // This class handles the player'
     private PlayerMovement _playerMovement;
 
     public float cooldownDurationFireBall = 1f;
-    private float cooldownTimerFireBall = 0f;                                                                       
+    private float cooldownTimerFireBall = 0f;
     public bool IsReadyFireBall => cooldownTimerFireBall <= 0f;
 
     public float cooldownDurationKick = 2f;
     private float cooldownTimerKick = 0f;
     public bool IsReadyKick => cooldownTimerKick <= 0f;
+
+    private Vector2 direction;
 
     void Start()
     {
@@ -77,19 +79,19 @@ public class AbilityController : MonoBehaviour // This class handles the player'
         Input.mousePosition.y,
         _mainCamera.nearClipPlane
         ));
-        
-        
+
+
         if (cooldownTimerFireBall > 0f)
-         cooldownTimerFireBall -= Time.deltaTime;   
-           
+            cooldownTimerFireBall -= Time.deltaTime;
+
         if (cooldownTimerKick > 0f)
-         cooldownTimerKick -= Time.deltaTime;  
-      
-        
+            cooldownTimerKick -= Time.deltaTime;
+
+
         mousePos.z = 0f;
 
-        Vector2 direction = (mousePos - firePoint.position).normalized; // Calculate the direction from the firePoint to the mouse position.
-        
+        direction = (mousePos - firePoint.position).normalized; // Calculate the direction from the firePoint to the mouse position.
+
         // RMB: ataque melee
         if (Input.GetMouseButtonDown(0))
         {
@@ -109,6 +111,12 @@ public class AbilityController : MonoBehaviour // This class handles the player'
         // LMB: bola de fuego
         if (Input.GetMouseButtonDown(1))
         {
+            ShootFireBall();
+        }
+    }
+
+    private void ShootFireBall()
+    {
             if (!IsReadyFireBall) return; // no disparar si está en cooldown
 
             cooldownTimerFireBall = cooldownDurationFireBall;
@@ -118,6 +126,8 @@ public class AbilityController : MonoBehaviour // This class handles the player'
             _fireballAbility.UseAbility(firePoint, direction);
             animatorController.TriggerAnim("fireball");
             PlayerEventsManager.Instance.PlayerFireball();
-        }
     }
+
+
+
 }
