c6cb02d4b9f7e543d49dc84d223a5013e12e484a
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-07-01 19:41:11 -0300

    Change fireball fire point

--- Assets/Scripts/Player/Attack Ability/Projectile.cs
+++ Assets/Scripts/Player/Attack Ability/Projectile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -8,9 +9,18 @@ public class Projectile : MonoBehaviour   // This script handles the behavior of
     public GameObject impactEffect;
     public float projectileSpeed = 5f;
     public int damage = 5;
-
+        
     private Rigidbody2D _rb;
+    private Collider2D _col;
     [SerializeField] bool canHitWalls;
+
+    void Awake()
+    {
+        _col = GetComponent<Collider2D>();
+        if (_rb == null) Debug.LogError("Projectile attached to " + gameObject.name + " has no Collider");
+
+        _col.enabled = false;
+    }
 
     private void Start()
     {
@@ -19,13 +29,25 @@ public class Projectile : MonoBehaviour   // This script handles the behavior of
 
         _rb.velocity = _rb.velocity * projectileSpeed;
 
+        StartCoroutine(EnableCollision());
+
         Destroy(gameObject, lifetime);
+    }
+
+    private IEnumerator EnableCollision()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _col.enabled = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)   // Called when the projectile enters a trigger collider.
     {
         if (collision.CompareTag("Room")) return;
-            Vector2 hitPoint = collision.ClosestPoint(transform.position);
+        Vector2 hitPoint = collision.ClosestPoint(transform.position);
+        if (impactEffect != null && canHitWalls)   // If there's an impact effect assigned, spawn it at the projectile's current position.
+        {
+            Instantiate(impactEffect, hitPoint, Quaternion.identity);
+        }
 
         if (collision.CompareTag("Enemy"))   // If the projectile hits an object tagged "Enemy", destroy the projectile.
         {
@@ -35,7 +57,7 @@ public class Projectile : MonoBehaviour   // This script handles the behavior of
             if (enemyHealth != null) enemyHealth.TakeDamage(damage);
             else Debug.LogError("enemy health not found");
 
-                // Instancia el efecto en ese punto            
+            // Instancia el efecto en ese punto            
             if (impactEffect != null)   // If there's an impact effect assigned, spawn it at the projectile's current position.
             {
                 Instantiate(impactEffect, hitPoint, Quaternion.identity);
@@ -43,10 +65,6 @@ public class Projectile : MonoBehaviour   // This script handles the behavior of
 
         }
 
-        if (impactEffect != null && canHitWalls)   // If there's an impact effect assigned, spawn it at the projectile's current position.
-        {
-            Instantiate(impactEffect, hitPoint, Quaternion.identity);
-        }
 
         Destroy(gameObject);   // Destroy the projectile after triggering the effect or hitting something.
 
