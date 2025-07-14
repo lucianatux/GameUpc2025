5c981116569d92c2a40a59bc2b07c2a33d911ce1
camilab <boessca@gmail.com>
2025-07-14 14:17:40 -0300

    Implement burger prop

---
+++ Assets/Scripts/EnvironmentScripts/PropHit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropHit : MonoBehaviour
+{
+
+    [SerializeField] GameObject hitImpact;
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerProjectile"))
+        {
+            if (hitImpact != null) Instantiate(hitImpact);
+
+            anim.SetTrigger("hit");
+        }
+    }
+}
