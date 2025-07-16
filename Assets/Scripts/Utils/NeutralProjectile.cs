ccc3fc0604fbc85dcfc2088c31ac22c09f872e20
Laugabr <125941878+Laugabr@users.noreply.github.com>
2025-07-16 03:48:13 -0300

    fix geyser hitbox

--- Assets/Scripts/Utils/NeutralProjectile.cs
+++ Assets/Scripts/Utils/NeutralProjectile.cs
@@ -7,9 +7,13 @@ public class NeutralProjectile : MonoBehaviour   // This script handles the beha
     public float lifetime = 3f;
     public GameObject impactEffect;
     public float projectileSpeed = 5f;
-    private Rigidbody2D _rb;
     public int damage;
+    public float damageInterval = 0.5f;  // tiempo entre cada daño en contacto
     [SerializeField] bool _canHitWalls;
+    
+    private Rigidbody2D _rb;
+    private float timer;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -69,7 +73,7 @@ public class NeutralProjectile : MonoBehaviour   // This script handles the beha
 
         if (impactEffect != null)   // If there's an impact effect assigned, spawn it at the projectile's current position.
         {
-            Instantiate(impactEffect, transform.position, Quaternion.identity);
+            Instantiate(impactEffect, hitPoint, Quaternion.identity);
             Destroy(impactEffect, 2f);
         }
 
@@ -77,8 +81,50 @@ public class NeutralProjectile : MonoBehaviour   // This script handles the beha
         Debug.Log("Choca con " + collision.gameObject.name);
     }
 
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        Vector2 hitPoint = collision.ClosestPoint(transform.position);
+        
+        if (collision.CompareTag("Player") || collision.CompareTag("Enemy"))
+        {
+            timer += Time.deltaTime;
+            if (timer >= damageInterval)
+            {
+                if (collision.CompareTag("Player"))
+                {
+                    PlayerHealth playerHealth = collision.GetComponent<PlayerHealth>();
+                    if (playerHealth != null) playerHealth.TakeDamage(damage);
+                    if (impactEffect != null)   // If there's an impact effect assigned, spawn it at the projectile's current position.
+                    {
+                        Instantiate(impactEffect, hitPoint, Quaternion.identity);
+                    }
+                }
+
+                else if (collision.CompareTag("Enemy"))
+                {
+                    EnemyHealth enemyHealth = collision.GetComponent<EnemyHealth>();
+                    if (enemyHealth != null) enemyHealth.TakeDamage(damage);
+                    if (impactEffect != null)   // If there's an impact effect assigned, spawn it at the projectile's current position.
+                    {
+                        Instantiate(impactEffect, hitPoint, Quaternion.identity);
+                    }
+                }
+                timer = 0f;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("Enemy"))
+        {
+            timer = 0f;  // Reiniciás el timer al salir del trigger
+        }
+    }
+}
 
     
-}
+
 
     
